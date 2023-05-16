e9db5a2c5e6836035ddad135593103ea0982367e
alexgimenez02 <alex.gimenez.saludes@gmail.com>
2023-05-16 13:16:39 +0200

    chore: added trees and clouds

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const icons = document.querySelectorAll('.icon');
 const icon_strings = ["EMOTE%20V5%20112X112.png", "EMOTE%20V2%20112X112.png", "EMOTE%20V1%20112X112.png"];
 let i = 2;
 let j = 2;
-
 icons.forEach((icon) => {
 	icon.addEventListener('animationend', () => {
         
@@ -20,13 +19,12 @@ icons.forEach((icon) => {
             {
                 j = (j + 1) % icon_strings.length;
                 icon.src = icon_strings[j];
+                
             } 
 			icon.style.animation = 'move-up 2s ease-in-out, wiggle 3s ease-in-out infinite'; // Animation to move from bottom to middle and wiggle
 		}, 17000); // Wait for 17 seconds before starting the animation again
          
     });
-    
-    icon.src = icon_strings[i];
 	icon.style.animation = 'move-up 2s ease-in-out, wiggle 3s ease-in-out infinite'; // Initial animation to move from bottom to middle and wiggle
 });
 
